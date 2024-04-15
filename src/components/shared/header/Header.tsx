08c92fd66a07c6a1bd7ecad0764f9f69b2155303
TeonaTsiramua <tsiramuateona@gmail.com>
2024-04-15 17:01:19 +0400

    add useScrollHandler

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -1,30 +1,11 @@
-import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
+import { useScrollHandler } from '../../../hooks/useScrollHandler';
 
 import Nav from '../nav/Nav';
 import { Logo, StyledHeader } from './styles';
 
 function Header() {
-  const [prevScrollPos, setPrevScrollPos] = useState(0);
-  const [visible, setVisible] = useState(true);
-
-  useEffect(() => {
-    function handleScroll() {
-      const currentScrollPos = window.scrollY;
-      const isScrolledDown = prevScrollPos < currentScrollPos;
-      const isTop = currentScrollPos === 0;
-      const isBelowThreshold = currentScrollPos < 50;
-
-      setVisible(isTop || isBelowThreshold || !isScrolledDown);
-      setPrevScrollPos(currentScrollPos);
-    }
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [prevScrollPos]);
+  const visible = useScrollHandler();
 
   const variants = {
     open: { opacity: 1, y: 0, transition: { duration: 0.5 } },
---
+++ src/hooks/useScrollHandler.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+export function useScrollHandler() {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    function handleScroll() {
+      const currentScrollPos = window.scrollY;
+      const isScrolledDown = prevScrollPos < currentScrollPos;
+      const isTop = currentScrollPos === 0;
+      const isBelowThreshold = currentScrollPos < 50;
+
+      setVisible(isTop || isBelowThreshold || !isScrolledDown);
+      setPrevScrollPos(currentScrollPos);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollPos]);
+
+  return visible;
+}
