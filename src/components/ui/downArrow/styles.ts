328a37d621665860e9b57bb716e46d1bdfaaf066
TeonaTsiramua <tsiramuateona@gmail.com>
2024-05-02 12:09:52 +0400

    change arrow color

--- src/components/ui/downArrow/styles.ts
+++ src/components/ui/downArrow/styles.ts
@@ -3,4 +3,6 @@ import styled from 'styled-components';
 export const Icon = styled.img`
   width: 20px;
   margin: 1rem 0;
+  filter: invert(15%) sepia(72%) saturate(984%) hue-rotate(170deg)
+    brightness(97%) contrast(90%);
 `;
