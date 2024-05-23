753bdfdf9ef5cb382f22a26e7d4cee4c096a4074
TeonaTsiramua <tsiramuateona@gmail.com>
2024-05-24 01:22:35 +0400

    update MenuToggle component

--- src/components/shared/nav/MenuToggle.tsx
+++ src/components/shared/nav/MenuToggle.tsx
@@ -1,4 +1,18 @@
+import { SVGMotionProps, motion } from 'framer-motion';
 import { SButton } from './styles';
+
+const Path = (props: SVGMotionProps<SVGPathElement>) => (
+  <motion.path
+    fill='transparent'
+    strokeWidth='3'
+    stroke='hsl(203, 78%, 27%)'
+    strokeLinecap='round'
+    transition={{
+      stroke: { duration: 0.7 },
+    }}
+    {...props}
+  />
+);
 
 export const MenuToggle = ({
   isMenuOpen,
@@ -9,41 +23,33 @@ export const MenuToggle = ({
 }) => (
   <SButton onClick={() => setIsMenuOpen(!isMenuOpen)}>
     <svg width='23' height='23' viewBox='0 0 23 23'>
-      {/* First Path */}
-      <path
-        fill='transparent'
-        strokeWidth='3'
-        stroke={isMenuOpen ? 'hsl(0, 0%, 95%)' : 'hsl(203, 78%, 27%)'}
-        strokeLinecap='round'
-        d={isMenuOpen ? 'M 3 16.5 L 17 2.5' : 'M 2 2.5 L 20 2.5'}
-        style={{
-          transition: 'stroke 0.5s ease-in-out, d 0.5s ease-in-out',
-          WebkitTransition: 'stroke 0.5s ease-in-out, d 0.5s ease-in-out',
+      <Path
+        animate={isMenuOpen ? 'open' : 'closed'}
+        variants={{
+          closed: {
+            d: 'M 2 2.5 L 20 2.5',
+            stroke: 'hsl(203, 78%, 27%)',
+          },
+          open: {
+            d: 'M 3 16.5 L 17 2.5',
+            stroke: 'hsl(0, 0%, 95%)',
+          },
         }}
       />
-      {/* Second Path */}
-      <path
-        fill='transparent'
-        strokeWidth='3'
-        stroke='hsl(203, 78%, 27%)'
-        strokeLinecap='round'
-        opacity={isMenuOpen ? 0 : 1}
+      <Path
         d='M 2 9.423 L 20 9.423'
-        style={{
-          transition: 'opacity 0.1s ease-in-out',
-          WebkitTransition: 'opacity 0.1s ease-in-out',
+        animate={isMenuOpen ? 'open' : 'closed'}
+        variants={{
+          closed: { opacity: 1 },
+          open: { opacity: 0 },
         }}
+        transition={{ duration: 0.1 }}
       />
-      {/* Third Path */}
-      <path
-        fill='transparent'
-        strokeWidth='3'
-        stroke={isMenuOpen ? 'hsl(0, 0%, 95%)' : 'hsl(203, 78%, 27%)'}
-        strokeLinecap='round'
-        d={isMenuOpen ? 'M 3 2.5 L 17 16.346' : 'M 2 16.346 L 20 16.346'}
-        style={{
-          transition: 'stroke 0.5s ease-in-out, d 0.5s ease-in-out',
-          WebkitTransition: 'stroke 0.5s ease-in-out, d 0.5s ease-in-out',
+      <Path
+        animate={isMenuOpen ? 'open' : 'closed'}
+        variants={{
+          closed: { d: 'M 2 16.346 L 20 16.346', stroke: 'hsl(203, 78%, 27%)' },
+          open: { d: 'M 3 2.5 L 17 16.346', stroke: 'hsl(0, 0%, 95%)' },
         }}
       />
     </svg>
