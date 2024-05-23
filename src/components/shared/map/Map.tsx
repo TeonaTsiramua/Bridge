ebf284387c6dd725c4315a02ccaa2ba873781772
TeonaTsiramua <tsiramuateona@gmail.com>
2024-05-24 00:36:06 +0400

    update map

--- src/components/shared/map/Map.tsx
+++ src/components/shared/map/Map.tsx
@@ -23,17 +23,14 @@ function MapComponent() {
           mapId='51a0bcbfd9b2d20f'
           defaultZoom={15}
           defaultCenter={{ lat: 41.7851, lng: 44.8226 }}
+          streetViewControl={false}
         />
         <AdvancedMarker
           position={{ lat: 41.7851, lng: 44.8226 }}
           title={'გობრონიძის ქუჩა 25ბ.'}
           onClick={handleMarkerClick}
         >
-          <Pin
-            background={'#0f527c'}
-            borderColor={' #0f527c'}
-            glyphColor={' #ffff'}
-          />
+          <Pin background='#0f527c' borderColor='#0f527c' glyphColor='#ffff' />
         </AdvancedMarker>
       </APIProvider>
     </Container>
