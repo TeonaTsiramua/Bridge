d2f2571cd2d900161814579243f8a0b801738189
TeonaTsiramua <tsiramuateona@gmail.com>
2024-05-24 20:02:32 +0400

    change post images

--- src/components/contact/posts/Posts.tsx
+++ src/components/contact/posts/Posts.tsx
@@ -9,14 +9,8 @@ const Posts = () => {
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
     >
-      <Img
-        src='https://scontent.ftbs3-2.fna.fbcdn.net/v/t39.30808-6/440963228_122128496846245862_148376517894505321_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=fpObufGLE3AQ7kNvgGlpeSE&_nc_ht=scontent.ftbs3-2.fna&oh=00_AYBci1b5ZoPeD2aD4JYvY1xuoJB_v70jfEStGFjdm-7rAg&oe=66454398'
-        alt=''
-      />
-      <Img
-        src='https://scontent.ftbs3-2.fna.fbcdn.net/v/t39.30808-6/433864519_122113979528245862_5287344982570157045_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=HzzuLIJ1FYIQ7kNvgEsfo7d&_nc_ht=scontent.ftbs3-2.fna&oh=00_AYCfIG_QBuyQ3mN1cKw9hvVmZOnPqJgRPwGGHlFqN2cC4g&oe=66454790'
-        alt=''
-      />
+      <Img src='/assets/posts/post1.jpg' alt='' />
+      <Img src='/assets/posts/post2.jpg' alt='' />
     </Container>
   );
 };
